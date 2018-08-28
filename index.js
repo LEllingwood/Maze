68443fa79f0dc2733085fa5beb9a9f03db3b68f5
Lea Ellingwood <leaellingwood@hotmail.com>
2018-08-28 16:21:46 -0400

    still waiting on wall stoppers

--- index.js
+++ index.js
@@ -15,52 +15,112 @@ const map = [
     "█░░░░░░░█░░░░░░░█░░░█",
     "█████████████████████"
 ];
-const wall = "█";
-const space = ░;
-let currentPlayer = "O"
+const gameboard = document.getElementById("gameboard");
+const playerElement = createPlayer();
+const playerPosition = {row: 9, column: 0}
 
-// let boxTop = 200;
-// let boxLeft = 200;
-// event handler for moving keys based on arrow press:
-// document.addEventListener('keydown', (event) => {
-//     let keyName = event.key;
-//     // console.log(event.key)
-//     if (keyName === "ArrowDown") {
-//         boxTop = boxTop + 10;
-//     }
-//     if (keyName === "ArrowUp") {
-//         boxTop = boxTop - 10;
-//     }
-//     if (keyName === "ArrowLeft") {
-//         boxLeft = boxLeft - 10;
-//     }
-//     if (keyName === "ArrowRight") {
-//         boxLeft = boxLeft + 10;
-//     }
+for (let rowIndex = 0; rowIndex < map.length; rowIndex++) {
+    const rowString = map[rowIndex];
+    const rowDiv = createRow();
+    gameboard.appendChild(rowDiv);
 
-//     document.getElementById("box").style.top = boxTop + "px";
-// //     document.getElementById("box").style.left = boxLeft + "px";
+    for (let cellIndex = 0; cellIndex < rowString.length; cellIndex++) {
+        const cellCharacter = rowString[cellIndex];
+        let cellElement = createCellElement(cellCharacter, rowIndex, cellIndex)
 
-
-
-function showGameBoard() {
-    for (let i = 0; i < map.length; i++) {
-        
+        if (cellElement.dataset.type === "start") {
+            movePlayerToCellElement(cellElement)
+        }
+        rowDiv.appendChild(cellElement)
     }
-
-    const board = document.createTextNode(map[0]);
-    const newP = document.createElement("div");
-    const destination = document.getElementById("row1");
-    newP.appendChild(board);
-    destination.appendChild(newP);
 }
-showGameBoard()
 
-function showGameBoard2() {
-    const board = document.createTextNode(map[1]);
-    const newP = document.createElement("div");
-    const destination = document.getElementById("row2");
-    newP.appendChild(board);
-    destination.appendChild(newP);
+function createRow() {
+    const rowDiv = document.createElement("div")
+    rowDiv.classList.add("row")
+    return rowDiv
 }
-showGameBoard2()
+
+function createPlayer() {
+    const playerDiv = document.createElement("div")
+    playerDiv.classList.add("player", "cell")
+    return playerDiv
+
+}
+
+function createCellElement(cellCharacter, rowIndex, cellIndex) {
+    const cellElement = document.createElement("div")
+    switch (cellCharacter) {
+        case "█":
+            cellElement.dataset.type = "wall"
+            break
+        case "░":
+            cellElement.dataset.type = "hall"
+            break
+        case "S":
+            cellElement.dataset.type = "start"
+            break
+        case "F":
+            cellElement.dataset.type = "finish"
+            break
+    }
+    cellElement.classList.add(cellElement.dataset.type, "cell")
+    cellElement.dataset.rowIndex = rowIndex;
+    cellElement.dataset.cellIndex = cellIndex;
+    return cellElement
+}
+// switch case is basically a big if else statement
+
+function movePlayerToCellElement(cellElement) {
+    cellElement.appendChild(playerElement)
+}
+
+
+function findCellByCoordinates(rowIndex, cellIndex) {
+    const rowIndexSelector = "[data-row-index='" + rowIndex + "']"
+    const cellIndexSelector = "[data-cell-index='" + cellIndex + "']"
+    const cellSelector = "#gameboard .row " + rowIndexSelector + cellIndexSelector
+    return document.querySelector(cellSelector)
+// this function will search the DOM for a cell that meets both the row and cell types
+// findCellByCoordinates(rowIndex, cellIndex + 1)-this is how you would check the cell to the right
+}
+
+
+// event handler for moving keys based on arrow press:
+document.addEventListener('keydown', (event) => {
+    let keyName = event.key;
+    let rowIndex = playerPosition.row
+    let cellIndex = playerPosition.column
+    if (keyName === "ArrowDown") {
+        console.log("hello")
+        let newCellNode = findCellByCoordinates(rowIndex + 1, cellIndex)
+        movePlayerToCellElement(newCellNode)
+        playerPosition.row +=1
+    }
+    if (keyName === "ArrowUp") {
+        let newCellNode = findCellByCoordinates(rowIndex - 1, cellIndex)
+        movePlayerToCellElement(newCellNode)
+        playerPosition.row -=1
+    }
+    if (keyName === "ArrowLeft") {
+        let newCellNode = findCellByCoordinates(rowIndex, cellIndex - 1)
+        movePlayerToCellElement(newCellNode)
+        playerPosition.column -=1
+    }
+    if (keyName === "ArrowRight") {
+        let newCellNode = findCellByCoordinates(rowIndex, cellIndex + 1)
+        movePlayerToCellElement(newCellNode)
+        playerPosition.column +=1
+    }
+    
+    
+})
+
+// function wallGuard(){
+//     if (cellElement.dataset.type === "wall") {
+//         alert("YOU SHALL NOT PASS!")
+//     }
+// } 
+// wallGuard()
+// code win message once player reaches position ___
+// can remove event listener once you reach finish so you can't move anymore
